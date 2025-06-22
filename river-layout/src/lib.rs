82251e5f1f8b7ac0984ea9713b7e0101c062ec67
Jason Fuchs <jasonfuchs@protonmail.com>
2025-06-22 09:24:13 +0200

    refactor: for now only use crate for protocol glue code

--- river-layout/src/lib.rs
+++ river-layout/src/lib.rs
@@ -1,2 +1,12 @@
-pub mod prelude;
-pub mod protocol;
+pub mod protocol {
+    use wayland_client;
+    use wayland_client::protocol::*;
+
+    pub mod __interfaces {
+        use wayland_client::protocol::__interfaces::*;
+        wayland_scanner::generate_interfaces!("protocol/river-layout-v3.xml");
+    }
+    use self::__interfaces::*;
+
+    wayland_scanner::generate_client_code!("protocol/river-layout-v3.xml");
+}
